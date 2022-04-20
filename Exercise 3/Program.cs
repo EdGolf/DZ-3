1eb7e733250e684efea32b000476590c2788ee70
Eduard <dirol2016@list.ru>
2022-04-20 12:08:22 +0500

    3

---
+++ Exercise 3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число N: ");
+double n = Convert.ToDouble(Console.ReadLine());
+double index = 1;
+
+while (index <= n)
+{
+    Console.WriteLine(Kub(index));
+    index++;
+}
+if (n < 0)
+    Console.WriteLine("Число N должно быть > 0!!!");
+    
+double Kub(double x)
+{
+   return  Math.Pow(x, 3);
+}
